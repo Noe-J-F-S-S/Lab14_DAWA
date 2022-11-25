a4ecf73ed21ecb98405aca801c9e0100f2f27bcc
Mac41 <mac41@MacBook-Pro-de-Mac41.local>
2022-11-25 13:45:51 -0500

    corrobora logeo y registro

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from "react";
+import { post } from "../../services"
 import TextField from '@mui/material/TextField';
+import Swal from "sweetalert2"
 import {
     Button,
     Card,
@@ -14,14 +16,57 @@ import {
   } from "@mui/material";
 
 const Login = () => {
+    const [values, setValues] = useState({
+        password: "",
+        email: ""
+      })
+      
+      const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setValues({
+          ...values,
+          [name]: value
+        })
+      }
+      
+      const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(values)
+        const response = await post("users/login", values);
+      
+        if(response.ok){
+          Swal.fire({
+            text: "Ususario logeado correctamente",
+            icon: "success",
+          })
+        } else {
+          Swal.fire({
+            text: "Usuario o contraseña incorrecto",
+            icon: "error",
+          });
+        }
+      }
 
   return (
     <Container>
         <form  noValidate autoComplete="off">
-        <TextField id="filled-basic" label="Email" variant="filled" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
+        <TextField 
+          name="email" 
+          
+          label="Email" 
+          value={values.email}
+          onChange={handleInputChange}
+           />
+        <TextField 
+          name="password" 
+          value={values.password}
+          onChange={handleInputChange}
+          id="outlined-basic" 
+          label="Password" 
+           />
         <Box my={2}>
-            <Button  size="large" variant="contained">
+            <Button  size="large" variant="contained"
+            onClick={handleSubmit}>
                 Ingresar
             </Button>
             <Button size="large" variant="contained">
